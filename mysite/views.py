7a1a67207be03d36640b07506a66046933fddbb6
nnarziev95 <nnarziev@gmail.com>
2020-04-22 00:03:02 +0900

    data selection for visualization modified

--- mysite/views.py
+++ mysite/views.py
@@ -12,7 +12,7 @@ from konlpy.tag import Okt
 import nltk
 
 # Create your views here.
-from mysite.models import Words
+from mysite.models import Words, Bigrams
 
 
 class Home(TemplateView):
@@ -57,7 +57,7 @@ def process_file(filename):
         year = int(reg_exp_date.search(filename).group().split('.')[0])
 
     parsed = parser.from_file('./media/' + filename)
-    # nltk_function(parsed["content"])
+    nltk_function(parsed["content"], year)
     # word2vec_function(parsed["content"].replace('\n', ' '))
     words_array = re.findall('\\w+', parsed["content"].replace('\n', ''))
 
@@ -79,15 +79,28 @@ def save_words(words_array, year):
     return result
 
 
-def nltk_function(doc_ko):
+def nltk_function(doc_ko, year):
     bigram_measures = nltk.collocations.BigramAssocMeasures()
     tokens = nltk.regexp_tokenize(doc_ko, '\\w+')
     finder = nltk.collocations.BigramCollocationFinder.from_words(tokens)
     finder.apply_freq_filter(4)
-    print(finder.nbest(bigram_measures.pmi, 30))
+    # print(finder.nbest(bigram_measures.pmi, 30))
     scored = finder.score_ngrams(bigram_measures.raw_freq)
     sorted_list = sorted(scored, key=lambda item: item[1])
-    print(sorted_list)
+    print(sorted_list.__len__())
+
+    bigrams = finder.nbest(bigram_measures.pmi, 50)
+    print(bigrams)
+
+    for bigram in bigrams:
+        new_bigram, created = Bigrams.objects.get_or_create(text=" ".join(map(str, bigram)), year=year)
+        if created:
+            new_bigram.count = 1
+            new_bigram.save()
+        else:
+            new_bigram.count += 1
+            new_bigram.save()
+
     # print(scored)
     # scores = sorted(score for bigram, score in scored)
     # bigrams = sorted(bigram for bigram, score in scored)
@@ -130,7 +143,7 @@ def db_view(request):
 
         year = request.POST['year']
 
-        data = Words.objects.all().filter(year=year).order_by('-count').values('text', 'count', 'year')
+        data = Words.objects.all().filter(year__range=[int(year) - 12 + 1, int(year)]).order_by('text', '-count', 'year').values('text', 'count', 'year')
         if data.__len__() == 0:
             return render(request, 'no_data.html')
 
@@ -156,11 +169,61 @@ def visualize(request):
 
         word_freq = {}
 
-        data_years = Words.objects.filter(year__range=[int(year) - chart_years_count, int(year)]).values_list('year', flat=True).distinct()
+        year_start = 2010
+        year_end = 2027
+        top5_words_for_sel_year = Words.objects.filter(year=year).order_by('-count')[:5]
+        words = []
+        final_data = []
+        for word in top5_words_for_sel_year:
+            print(word.text)
+            words.append(Words.objects.filter(text=word.text, year__range=[year_start, year_end]).order_by('year'))
+
+        for w in words:
+            for a in w:
+                print(a.text + " " + str(a.year) + " " + str(a.count))
+
+        dif_years = False
+        for w in words:
+            counter_year = year_start
+            counter_words = 0
+            for i in range(0, (year_end - year_start) + 1):
+                if counter_year > year_end:
+                    break
+
+                if counter_words < len(w):
+                    if w[counter_words].year == counter_year:
+                        # final_data.append({"text": w[counter_words].text, "year": w[counter_words].year, "count": w[counter_words].count})
+                        txt = w[counter_words].text
+                        yr = w[counter_words].year
+                        cnt = w[counter_words].count
+
+                        if not dif_years:
+                            counter_words = i + 1
+                        else:
+                            counter_words = counter_words + 1
+                        # dif_years = False
+                    else:
+                        dif_years = True
+                        # final_data.append({"text": w[counter_words].text, "year": counter_year, "count": 0})
+                        txt = w[counter_words].text
+                        yr = counter_year
+                        cnt = 0
+                else:
+                    # final_data.append({"text": w[counter_words].text, "year": counter_year, "count": 0})
+                    txt = w[counter_words - 1].text
+                    yr = counter_year
+                    cnt = 0
+
+                final_data.append({"text": txt, "year": yr, "count": cnt})
+                counter_year += 1
+
+        print(final_data)
+        data_years = Words.objects.filter(year__range=[int(year) - chart_years_count + 1, int(year)]).values_list('year', flat=True).distinct()
         data_years = sorted(data_years)
         data_years_count = data_years.__len__()
+        print(data_years_count)
 
-        chart_data = Words.objects.all().filter(year__range=[int(year) - chart_years_count, int(year)]).order_by('-count', 'text', 'year').values('text', 'count', 'year')[:chart_data_count * data_years_count]
+        chart_data = Words.objects.all().filter(year__range=[int(year) - chart_years_count + 1, int(year)]).order_by('-count', 'text', 'year').values('text', 'count', 'year')[:chart_data_count * data_years_count]
         wc_data = Words.objects.all().filter(year=year).order_by('-count').values('text', 'count', 'year')[:word_cloud_count]
 
         if chart_data.__len__() == 0:
